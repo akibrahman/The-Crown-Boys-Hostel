74139024b1ee2d2a278fbd9be4fa6b1887f2b3b5
akibrahman <akibrahman5200@gmail.com>
2024-02-21 16:46:20 +0600

    Login page modified

---
+++ src/app/logIn/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "LogIn",
+  description: "This is Login page",
+};
+
+const layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default layout;
