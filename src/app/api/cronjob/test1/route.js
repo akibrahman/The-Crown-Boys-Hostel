41cebadeed0f0fda1364ad25c4442adf8e837091
akibrahman <akibrahman5200@gmail.com>
2024-02-22 11:04:13 +0600

    Test Cron Job Added 6

--- src/app/api/cronjob/test1/route.js
+++ src/app/api/cronjob/test1/route.js
@@ -2,38 +2,42 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 export const GET = async (req) => {
-  if (
-    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return res.status(401).end("Unauthorized");
+  try {
+    if (
+      req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+      return res.status(401).end("Unauthorized");
+    }
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: "cron-job@hostelplates.com",
+      to: "akibrahman5200@gmail.com",
+      subject: "Cron Job",
+      html: `<div>
+        <p>This is the Prove</p>
+        <p>To Date String :${new Date().toDateString()}</p>
+        <p>To Time String :${new Date().toTimeString()}</p>
+        <p>To Local Date String :${new Date().toLocaleDateString()}</p>
+        <p>To Local Time String :${new Date().toLocaleTimeString()}</p>
+        <p>To String :${new Date().toString()}</p>
+        <p>To Local String :${new Date().toLocaleString()}</p>
+        <p>To ISOS String :${new Date().toISOString()}</p>
+        </div>`,
+    };
+
+    await transport.sendMail(mailOptions);
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
   }
-  const transport = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_APP_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: "cron-job@hostelplates.com",
-    to: "akibrahman5200@gmail.com",
-    subject: "Cron Job",
-    html: `<p>
-    <p>This is the Prove</p>
-    <p>To Date String :${new Date().toDateString()}</p>
-    <p>To Time String :${new Date().toTimeString()}</p>
-    <p>To Local Date String :${new Date().toLocaleDateString()}</p>
-    <p>To Local Time String :${new Date().toLocaleTimeString()}</p>
-    <p>To String :${new Date().toString()}</p>
-    <p>To Local String :${new Date().toLocaleString()}</p>
-    <p>To ISOS String :${new Date().toISOString()}</p>
-    </>`,
-  };
-
-  await transport.sendMail(mailOptions);
-  return NextResponse.json({ success: true });
 };
