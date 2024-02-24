5361be4a17c777f1030b95d974f60fa7f0a92418
akibrahman <akibrahman5200@gmail.com>
2024-02-24 20:04:09 +0600

    Vercel local date checking 10

--- src/app/api/cronjob/test1/route.js
+++ src/app/api/cronjob/test1/route.js
@@ -46,43 +46,43 @@ export const GET = async (req) => {
     }
     const testData = isSecondLastDayOfCurrentMonthInBangladesh();
 
-    // if (testData.isSecondLastDay) {
-    const currentDate = new Date().toLocaleString("en-US", {
-      timeZone: "Asia/Dhaka",
-    });
-    const currentMonth = new Date(currentDate).getMonth();
-    const nextNextMonthNumber = new Date(currentDate).getMonth() + 2;
-    const currentYear = new Date(currentDate).getFullYear();
-    const nextMonth = new Date(
-      currentYear,
-      currentMonth + 1,
-      1
-    ).toLocaleDateString("en-BD", {
-      month: "long",
-      timeZone: "Asia/Dhaka",
-    });
-    const dayCountOfNextMonth = parseInt(
-      new Date(currentYear, nextNextMonthNumber, 0).getDate()
-    );
+    if (testData.isSecondLastDay) {
+      const currentDate = new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Dhaka",
+      });
+      const currentMonth = new Date(currentDate).getMonth();
+      const nextNextMonthNumber = new Date(currentDate).getMonth() + 2;
+      const currentYear = new Date(currentDate).getFullYear();
+      const nextMonth = new Date(
+        currentYear,
+        currentMonth + 1,
+        1
+      ).toLocaleDateString("en-BD", {
+        month: "long",
+        timeZone: "Asia/Dhaka",
+      });
+      const dayCountOfNextMonth = parseInt(
+        new Date(currentYear, nextNextMonthNumber, 0).getDate()
+      );
 
-    // for (let i = 1; i <= dayCountOfNextMonth; i++) {
-    //   const newOrder = new Order({
-    //     userId: "Here the user ID",
-    //     managerId: "Here the manager ID",
-    //     month: nextMonth,
-    //     year: currentYear,
-    //     date: new Date(currentYear, currentMonth, i).toLocaleDateString(),
-    //     breakfast: false,
-    //     lunch: false,
-    //     dinner: false,
-    //   });
-    //   await newOrder.save();
-    // }
-    const mailOptions = {
-      from: "cron-job@hostelplates.com",
-      to: "akibrahman5200@gmail.com",
-      subject: "Cron Job",
-      html: `<div>
+      // for (let i = 1; i <= dayCountOfNextMonth; i++) {
+      //   const newOrder = new Order({
+      //     userId: "Here the user ID",
+      //     managerId: "Here the manager ID",
+      //     month: nextMonth,
+      //     year: currentYear,
+      //     date: new Date(currentYear, currentMonth, i).toLocaleDateString(),
+      //     breakfast: false,
+      //     lunch: false,
+      //     dinner: false,
+      //   });
+      //   await newOrder.save();
+      // }
+      const mailOptions = {
+        from: "cron-job@hostelplates.com",
+        to: "akibrahman5200@gmail.com",
+        subject: "Cron Job",
+        html: `<div>
           <p>Is Last Day:${testData.isSecondLastDay}</p>
           <p>Next Month :${nextMonth}</p>
           <p>Current Year :${currentYear}</p>
@@ -90,12 +90,14 @@ export const GET = async (req) => {
             currentYear,
             currentMonth,
             dayCountOfNextMonth
-          ).toLocaleDateString()}</p>
+          ).toLocaleDateString("en-BD", {
+            timeZone: "Asia/Dhaka",
+          })}</p>
           <p>Day Count of Next Month :${dayCountOfNextMonth}</p>
           </div>`,
-    };
-    await transport.sendMail(mailOptions);
-    // }
+      };
+      await transport.sendMail(mailOptions);
+    }
 
     return NextResponse.json({ success: true });
   } catch (error) {
