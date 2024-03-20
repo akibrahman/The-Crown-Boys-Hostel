372ff1e1683cd0ca99947b0b9684fe357680c28e
akibrahman <akibrahman5200@gmail.com>
2024-03-20 17:14:47 +0600

    Clients fetching in userQuery

--- src/app/userQuery/page.jsx
+++ src/app/userQuery/page.jsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { AuthContext } from "@/providers/ContextProvider";
 import axios from "axios";
+import { useContext } from "react";
 
 const UserQuery = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="max-h-screen">
       <p className="text-center font-semibold text-2xl">User Query</p>
@@ -21,7 +24,10 @@ const UserQuery = () => {
         </select>
         <button
           onClick={async () => {
-            await axios.get("/api/clients/getclients2?email=akib");
+            const { data } = await axios.get(
+              `/api/clients/getclients?id=${user?._id}`
+            );
+            console.log(">>>", data);
           }}
           className="bg-yellow-500 px-4 py-2 rounded-md duration-300 font-semibold text-stone-800 active:scale-90"
         >
