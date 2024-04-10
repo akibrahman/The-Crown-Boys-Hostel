e8ff598d659bc6246a61c95f4829749733c57cfe
akibrahman <akibrahman5200@gmail.com>
2024-04-10 11:46:37 +0600

    Verifing page updated

--- src/app/verifyemail/page.js
+++ src/app/verifyemail/page.js
@@ -4,14 +4,19 @@ import { AuthContext } from "@/providers/ContextProvider";
 import axios from "axios";
 import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
+import { CgSpinner } from "react-icons/cg";
+import { FaTimes } from "react-icons/fa";
+import { TiTick } from "react-icons/ti";
 
 const VerifyEmail = () => {
   const { userRefetch } = useContext(AuthContext);
   const [token, setToken] = useState("");
   const [verified, setVerified] = useState(false);
   const [error, setError] = useState(false);
+  const [verifing, setVerifing] = useState(false);
 
   const verifyUserEmail = async () => {
+    setVerifing(true);
     try {
       const { data } = await axios.post("/api/verifyemail", { token });
       if (data.success) {
@@ -23,6 +28,8 @@ const VerifyEmail = () => {
     } catch (error) {
       setError(true);
       console.log(error.response.data);
+    } finally {
+      setVerifing(false);
     }
   };
 
@@ -32,27 +39,69 @@ const VerifyEmail = () => {
   }, []);
 
   useEffect(() => {
-    if (token.length > 0) {
+    if (token?.length > 0) {
       verifyUserEmail();
     }
   }, [token]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <p>Verify E-mail</p>
-      <p>{token ? "Token is here" : "No token"}</p>
-      {verified && (
-        <div>
-          <p>Email Verified</p>
-          <Link href={"/"}>Go Home</Link>
-        </div>
-      )}
-      {error && (
-        <div>
-          <p>Verification Error, try again!</p>
-          <Link href={"/profile"}>Go Profile</Link>
-        </div>
-      )}
+    <div className="flex flex-col md:flex-row items-center justify-center gap-5 min-h-screen py-2 dark:bg-stone-900 dark:text-white font-semibold">
+      <div className="text-right">
+        <p className="text-5xl font-bold mb-3">Manager Expo</p>
+        <p className="text-[12px]">A Quality Meal Management System</p>
+        <p className="text-[12px] text-sky-500 leading-none">
+          Designed & Developed by{" "}
+          <a href="https://portfolio-akib.web.app" target="_blank">
+            <span className="font-bold underline">Akib Rahman</span>
+          </a>
+        </p>
+      </div>
+      <div>
+        <p className="text-blue-500">Token Verifing Section</p>
+        {verifing ? (
+          <p className="flex items-center gap-2 text-lime-500">
+            Verifing Token{" "}
+            <CgSpinner
+              className="text-xl animate-spin
+            "
+            />
+          </p>
+        ) : verified ? (
+          <>
+            <p className="flex items-center gap-2 text-green-500">
+              Token Verified <TiTick />
+            </p>
+            <Link href={"/profile"}>
+              <button className="px-8 text-white py-1 rounded-full my-2 duration-300 active:scale-90 bg-green-500">
+                Profile
+              </button>
+            </Link>
+          </>
+        ) : error ? (
+          <>
+            <p className="flex items-center gap-2 text-red-500">
+              Verification Error, try again! <FaTimes />
+            </p>
+            <Link href={"/signin"}>
+              <button className="px-8 text-white py-1 rounded-full my-2 duration-300 active:scale-90 bg-orange-500">
+                Login
+              </button>
+            </Link>
+          </>
+        ) : null}
+        {/* {!verified && (
+          <div>
+            <p>Email Verified</p>
+            <Link href={"/"}>Go Home</Link>
+          </div>
+        )}
+        {error && (
+          <div>
+            <p>Verification Error, try again!</p>
+            <Link href={"/profile"}>Go Profile</Link>
+          </div>
+        )} */}
+      </div>
     </div>
   );
 };
