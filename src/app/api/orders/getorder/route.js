2bc8d98517e1385c8f4169828ca62f2f9cf1cb70
akibrahman <akibrahman5200@gmail.com>
2024-04-17 00:47:49 +0600

    Order error debugging 6

--- src/app/api/orders/getorder/route.js
+++ src/app/api/orders/getorder/route.js
@@ -11,9 +11,11 @@ export const POST = async (req) => {
     const order = await Order.find({ userId, date });
     if (order.length > 0)
       return NextResponse.json({ msg: "OK", success: true, order: order[0] });
-    else throw new Error("Order not found");
+    else {
+      console.log("++=>", order, userId, date);
+      throw new Error("Order not found");
+    }
   } catch (error) {
-    console.log(error);
     return NextResponse.json({ msg: "Backend Error", error }, { status: 400 });
   }
 };
