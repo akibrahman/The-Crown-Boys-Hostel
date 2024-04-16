b8f98e9cb4c9108b7ded3bcf2394d5628f02b1e6
akibrahman <akibrahman5200@gmail.com>
2024-04-17 00:27:25 +0600

    Order error debugging 2

--- src/app/api/orders/getorder/route.js
+++ src/app/api/orders/getorder/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export const POST = async (req) => {
   try {
     const { userId, date } = await req.json();
+    console.log("Order Changed");
     const order = await Order.findOne({ userId, date });
     if (order) return NextResponse.json({ msg: "OK", success: true, order });
     else throw new Error("Order not found");
