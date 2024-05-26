8c1cccdda8a78d2b16d0f8f6bc1fa04f5b34627e
akibrahman <akibrahman5200@gmail.com>
2024-05-26 21:26:50 +0600

    End point created for scanner

---
+++ src/app/api/orders/scanner/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+const { dbConfig } = require("@/dbConfig/dbConfig");
+
+await dbConfig();
+
+export const GET = async (req) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const tagId = searchParams.get("tagId");
+    const meal = searchParams.get("meal");
+    const date = searchParams.get("date");
+    const month = searchParams.get("month");
+    const year = searchParams.get("year");
+    if (!tagId || !meal || !date || !month || !year) {
+      return NextResponse.json({ code: 4001, success: false });
+    }
+    return NextResponse.json({
+      code: 2000,
+      success: true,
+      tagId,
+      meal,
+      date,
+      month,
+      year,
+    });
+  } catch (error) {
+    return NextResponse.json({ code: 4002, success: false });
+  }
+};
