95bc12ddfb6fc2dd069585146fc890320c965d1b
akibrahman <akibrahman5200@gmail.com>
2024-06-01 11:59:16 +0600

    RFID scanner - making api endpoint 2

--- src/app/api/rfid/scanner/route.js
+++ src/app/api/rfid/scanner/route.js
@@ -7,7 +7,18 @@ export const GET = async (req) => {
   try {
     const { searchParams } = new URL(req.url);
     const cardId = searchParams.get("cardId");
-    return NextResponse.json({ cardId, success: true });
+    const mela = searchParams.get("mela");
+    const date = searchParams.get("date");
+    const month = searchParams.get("month");
+    const year = searchParams.get("year");
+    return NextResponse.json({
+      cardId,
+      meal,
+      date,
+      month,
+      year,
+      success: true,
+    });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ success: false, error }, { status: 500 });
