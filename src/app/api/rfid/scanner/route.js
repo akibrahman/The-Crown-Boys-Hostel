6d9dad0b79a588a2a56e3aab59212420054ae8e2
akibrahman <akibrahman5200@gmail.com>
2024-06-01 21:58:02 +0600

    RFID scanner - making api endpoint 4

--- src/app/api/rfid/scanner/route.js
+++ src/app/api/rfid/scanner/route.js
@@ -1,4 +1,7 @@
 import { dbConfig } from "@/dbConfig/dbConfig";
+import Order from "@/models/orderModel";
+import RFID from "@/models/rfid";
+import User from "@/models/userModel";
 import { NextResponse } from "next/server";
 
 await dbConfig();
@@ -11,12 +14,40 @@ export const GET = async (req) => {
     const date = searchParams.get("date");
     const month = searchParams.get("month");
     const year = searchParams.get("year");
+    if (!cardId || !meal || !date || !month || !year)
+      return NextResponse.json({ success: false, msg: "Missing Information" });
+    const dataBaseCard = await RFID.findOne({ cardId });
+    if (!dataBaseCard)
+      return NextResponse.json({ success: false, msg: "Invalid Card" });
+    if (!dataBaseCard.userId)
+      return NextResponse.json({ success: false, msg: "Unregistered Card" });
+    const orderDate = month + "/" + date + "/" + year;
+    const order = await Order.findOne({
+      userId: dataBaseCard.userId,
+      date: orderDate,
+    });
+    const user = await User.findById(dataBaseCard.userId);
+    if (!order)
+      return NextResponse.json({ success: false, msg: "No Order Found" });
+    const mealMappingForGuestMealCount = {
+      breakfast: "guestBreakfastCount",
+      lunch: "guestLunchCount",
+      dinner: "guestDinnerCount",
+    };
+    const mealPropertyForGuestMeal =
+      mealMappingForGuestMealCount[meal.toLowerCase()];
+    const isMeal = order[meal] || order[mealPropertyForGuestMeal] > 0;
+    const count = order[mealPropertyForGuestMeal] + (order[meal] ? 1 : 0);
+    if (!isMeal)
+      return NextResponse.json({
+        success: false,
+        msg: "You Haven't Order Meal",
+      });
     return NextResponse.json({
-      cardId,
-      meal,
-      date,
-      month,
-      year,
+      isMeal,
+      count,
+      msg: "Enjoy Your Meal",
+      name: user.username,
       success: true,
     });
   } catch (error) {
