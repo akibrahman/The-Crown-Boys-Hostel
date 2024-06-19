109af715062725d9c51a0157f390db0c5269c576
akibrahman <akibrahman5200@gmail.com>
2024-06-19 14:29:18 +0600

    Dashboard Obj hanged

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -22,8 +22,35 @@ const page = async () => {
 
   const user = await User.findById(id);
   console.log(user);
+  const plainUser={
+    _id:user._id,
+    username:user.username,
+    email:user.email,
+    contactNumber:user.contactNumber,
+    fathersNumber:user.fathersNumber,
+    mothersNumber:user.mothersNumber,
+    profilePicture:user.profilePicture,
+    role:user.role,
+    password:user.password,
+    isVerified:user.isVerified,
+    isManager:user.isManager,
+    isClient:user.isClient,
+    isManagerVerified:user.isManagerVerified,
+    isClientVerified:user.isClientVerified,
+    floor:user.floor,
+    roomNumber:user.roomNumber,
+    studentId:user.studentId,
+    bloodGroup:user.bloodGroup,
+    institute:user.institute,
+    messAddress:user.messAddress,
+    manager:user.manager,
+    nidFrontPicture:user.nidFrontPicture,
+    nidBackPicture:user.nidBackPicture,
+    charges:user.charges,
+    blockDate:user.blockDate,
+  }
 
-  return <Dashboard user={user.toJSON()}/>;
+  return <Dashboard user={plainUser}/>;
 };
 
 export default page;
