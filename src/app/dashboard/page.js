82a5c3439363a0e546edb594aa2c9fe4f56a0208
akibrahman <akibrahman5200@gmail.com>
2024-06-19 16:26:09 +0600

    Dashboard obj error 2

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,28 +1,31 @@
-"use client";
+"use server";
+import User from "@/models/userModel";
+import { decode, verify } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import Dashboard from "../../Components/Dashboard/Dashboard.jsx";
-import { useContext } from "react";
-import { AuthContext } from "@/providers/ContextProvider.jsx";
 
-const Page = () => {
-  // const token = cookies().get("token");
+const page = async () => {
+  const token = cookies().get("token");
 
-  // try {
-  //   verify(token.value, process.env.TOKEN_SECRET);
-  // } catch (err) {
-  //   console.log(err);
-  //   const baseUrl = process.env.BASE_URL;
-  //   const url = new URL("/signin", baseUrl);
-  //   url.searchParams.set("callbackUrl", baseUrl + "/dashboard");
-  //   return redirect(url);
-  // }
+  try {
+    verify(token.value, process.env.TOKEN_SECRET);
+  } catch (err) {
+    console.log(err);
+    const baseUrl = process.env.BASE_URL;
+    const url = new URL("/signin", baseUrl);
+    url.searchParams.set("callbackUrl", baseUrl + "/dashboard");
+    return redirect(url);
+  }
 
-  // const { id } = decode(token.value);
+  const { id } = decode(token.value);
 
-  // const user = await User.findById(id);
-  // console.log(user);
-  const { user } = useContext(AuthContext);
-  if (!user) return;
-  return <Dashboard user={user} />;
+  const user = await User.findById(id);
+  let temp = { ...user._doc, _id: user._id.toString() };
+  delete temp.files;
+  console.log(temp);
+
+  return <Dashboard user={temp} />;
 };
 
-export default Page;
+export default page;
