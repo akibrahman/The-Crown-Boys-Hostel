5c69448acb4ba4f7d21ce6916cabb9fc662a07bf
akibrahman <akibrahman5200@gmail.com>
2024-06-30 11:51:22 +0600

    blocked user block 4

--- src/app/userDetails/[id]/page.js
+++ src/app/userDetails/[id]/page.js
@@ -251,12 +251,7 @@ const page = async ({ params }) => {
                 <p className="text-red-600 font-semibold">Block Date:</p>
                 {client.blockDate ? (
                   <div className="flex items-center gap-3">
-                    <p>
-                      {new Date(client.blockDate).toDateString("en-BD", {
-                        // month: "long",
-                        timeZone: "Asia/Dhaka",
-                      })}
-                    </p>
+                    <p>{new Date(client.blockDate).toDateString()}</p>
                   </div>
                 ) : (
                   <div className="flex items-center gap-3">
