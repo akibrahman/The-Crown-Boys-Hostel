2b594d40af8caad2ea52e2b0850cebad88eb3d1c
akibrahman <akibrahman5200@gmail.com>
2024-09-13 23:41:52 +0600

    Reg error fixing 2

--- src/app/userDetails/edit/[id]/page.jsx
+++ src/app/userDetails/edit/[id]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import PreLoader from "@/Components/PreLoader/PreLoader";
+import UnderConstruction from "@/Components/UnderConstruction/UnderConstruction";
 import { base64 } from "@/utils/base64";
 import { imageUpload } from "@/utils/imageUpload";
 import { makeFile } from "@/utils/makeFile";
@@ -276,6 +277,8 @@ const Page = ({ params }) => {
       setIsChargeAdding(false);
     }
   };
+
+  return <UnderConstruction />;
 
   if (!client) return <PreLoader />;
 
