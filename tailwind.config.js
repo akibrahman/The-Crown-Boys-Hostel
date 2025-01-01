a3847e16c6c8caa3f6140de00f279eb01f76bd14
akibrahman <akibrahman5200@gmail.com>
2025-01-01 16:36:11 +0600

    Config.Tailwind Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   optimizeCss: false,
   enableBabelRuntime: true,
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
