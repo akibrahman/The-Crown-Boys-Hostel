f6a9deb9c033390d93df23a2ed480f5846ab2134
Akib Rahman <akibrahman5200@gmail.com>
2025-03-18 23:20:38 +0600

    camera

--- src/app/camera/page.js
+++ src/app/camera/page.js
@@ -1,32 +1,28 @@
 // pages/index.js
 "use client";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 
 const Home = () => {
   const socket = useRef(null);
   const videoRef = useRef(null);
+  const [image, setImage] = useState("/images/about/about-image-2-dark.svg");
 
   useEffect(() => {
-    socket.current = io("https://crown-camera-server.glitch.me");
+    socket.current = io("https://the-crown-socket-server.glitch.me");
   }, []);
 
   useEffect(() => {
-    socket.current.on("video-stream", async (data) => {
-      //
-      const blob = new Blob([data], { type: "image/jpeg" });
-      const url = URL.createObjectURL(blob);
-      console.log(url);
-      videoRef.current.src = url;
-      //
+    socket.current.on("image_upload", async (data) => {
+      setImage(`data:image/jpeg;base64,${data}`);
     });
   });
 
   return (
     <div>
       <h1>ESP32 CAM Video Stream</h1>
-      <Image ref={videoRef} alt="Video Stream" height={500} width={500} />
+      <Image src={image} alt="Video Stream" height={500} width={500} />
     </div>
   );
 };
