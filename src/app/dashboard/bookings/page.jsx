27b44d7de30c4fb7fc27d4bc64358540f4e9414e
Akib Rahman <akibrahman5200@gmail.com>
2025-04-02 17:40:20 +0600

    Bookings modified 2

--- src/app/dashboard/bookings/page.jsx
+++ src/app/dashboard/bookings/page.jsx
@@ -3,7 +3,7 @@ import PreLoader from "@/Components/PreLoader/PreLoader";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/providers/ContextProvider";
 import BookingComponent from "./BookingComponent";
 import { convertCamelCaseToCapitalized } from "@/utils/camelToCapitalize";
@@ -19,6 +19,8 @@ const ManagerAllBookingsComponent = () => {
   const roomFloor = searchParams.get("roomFloor");
   const roomName = searchParams.get("roomName");
   const roomBuilding = searchParams.get("roomBuilding");
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     if (!roomId || !bedNo || !roomFloor || !roomName || !roomBuilding)
@@ -42,6 +44,11 @@ const ManagerAllBookingsComponent = () => {
     enabled: user?._id ? true : false,
   });
 
+  // Filter bookings based on search term
+  const filteredBookings = bookings.filter((booking) =>
+    new RegExp(searchTerm, "i").test(booking?.name)
+  );
+
   if (!user || isLoading) return <PreLoader />;
   if (user?.success == false) return route.push("/signin");
   if (user.role != "manager") return route.push("/");
@@ -62,16 +69,25 @@ const ManagerAllBookingsComponent = () => {
         />
       )}
       <div className="min-h-full p-4 bg-dashboard text-slate-100">
-        <p className="text-center font-semibold mb-4 text-2xl dark:text-white">
-          Bookings
-        </p>
-        {bookings?.length == 0 && !isLoading && (
+        <div className="flex flex-col md:flex-row items-center justify-center gap-4 mb-4">
+          <p className="text-center font-semibold text-2xl dark:text-white">
+            Bookings
+          </p>
+          <input
+            type="text"
+            placeholder="Search by Name..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="p-2 rounded-md bg-gray-800 text-white outline-none"
+          />
+        </div>
+        {filteredBookings?.length == 0 && !isLoading && (
           <p className="text-center font-semibold text-slate-300">
             No Bookings
           </p>
         )}
         <div className="flex flex-col gap-10">
-          {bookings.map((booking, i) => (
+          {filteredBookings.map((booking, i) => (
             <BookingCard refetch={refetch} booking={booking} key={i} />
           ))}
         </div>
